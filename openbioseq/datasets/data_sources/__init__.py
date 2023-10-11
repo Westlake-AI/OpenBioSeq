dfaa2f1429638029cf9de086f8f49a78b65b4ca6
Lupin1998 <1070535169@qq.com>
2023-10-11 11:04:10 -1000

    fix bug

--- openbioseq/datasets/data_sources/__init__.py
+++ openbioseq/datasets/data_sources/__init__.py
@@ -1,11 +1,10 @@
 from .bio_seq_source import BioSeqDataset
 from .custom_source import ProcessedDataset
 from .dna_seq_source import DNASeqDataset
-from .genomic_source import GenomicDataset
 from .image_list import ImageList
 from .vision_source import CIFAR10, CIFAR100, CIFAR_C, MNIST, FMNIST, KMNIST, USPS
 
 __all__ = [
-    'BioSeqDataset', 'ProcessedDataset', 'DNASeqDataset', 'GenomicDataset',
+    'BioSeqDataset', 'ProcessedDataset', 'DNASeqDataset',
     'CIFAR10', 'CIFAR100', 'CIFAR_C', 'ImageList', 'MNIST', 'FMNIST', 'KMNIST', 'USPS',
 ]
